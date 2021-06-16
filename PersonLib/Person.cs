583a4fd04810435d8900ed9e7ad8948c8012593a
smasgl <masooglu.simon@gmail.com>
2021-06-16 11:01:57 +0200

    Initiate person library

---
+++ PersonLib/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Smasgl.Model.PersonLib
+{
+    public class Person
+    {
+        private string lastName;
+        private string firstname;
+
+        public Person() { }
+
+        public Person(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+        }
+
+        public string FirstName
+        {
+            get => this.firstname;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception($"{nameof(Person)}:{nameof(FirstName)}");
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception($"{nameof(Person)}:{nameof(LastName)}");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
